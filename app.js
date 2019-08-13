d57706839408bfb0fa361826ad521cd720511af6
Florian Barthel <florianbarthel@yahoo.de>
2019-08-13 14:29:21 +0200

    setup telnet server with docker

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const net = require('net')
+
+const newSocket = (socket) => {
+    socket.write('test telnet server')
+}
+
+var server = net.createServer(newSocket);
+
+server.listen(8888)
