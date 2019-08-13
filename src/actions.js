a77eace477ec0c6c29d71d36bd0a861e9bfe2d7c
Florian Barthel <florianbarthel@yahoo.de>
2019-08-13 22:26:46 +0200

    implement wall function

--- src/actions.js
+++ src/actions.js
@@ -19,7 +19,7 @@ const post = (name, content) => {
             time: Math.floor(Date.now() / 1000)
         })
         users.saveUsers(allUsers)
-        return 'Succesfully pushed to timeline.'
+        return name + ' has succesfully pushed to timeline.'
     } else {
         return errormessages.userNotFound(name)
     }
@@ -31,33 +31,76 @@ const post = (name, content) => {
  * @param {string} name 
  */
 const wall = (name) => {
-    //TODO:
-    //get all posts from user (name)
-    //get all posts from following users
-    //sort all posts by time
-    //print all posts in the correct order
+    var allPosts = []
+
+    //get all own posts from user (name)
+    const allUsers = users.loadUsers()
+    const user = allUsers.find((user) => user.name === name)
+    if (user) {
+        user.posts.forEach((post) => {
+            //push all posts with additional author attribute
+            allPosts.push({
+                author: name,
+                content: post.content,
+                time: post.time
+            })
+        })
+    
+        //get all users that the user (name) has subscribed
+        const allFollowingUsers = user.following
+        allFollowingUsers.forEach((followingUserName) => {
+            //get the subscribed user by name
+            followingUser = allUsers.find((findFollowingUser) => findFollowingUser.name === followingUserName)
+            //get all posts from subscribed users
+            followingUser.posts.forEach((post) => {
+                //push all posts with additional author attribute
+                allPosts.push({
+                    author: followingUserName,
+                    content: post.content,
+                    time: post.time
+                })
+            })
+        })
+    
+        //sort all posts by time
+        allPosts.sort((a, b) => {
+            return b.time - a.time
+        })
+    
+        //print all posts in the correct order
+        var result = 'Timeline from ' + name + ':\n'
+        allPosts.forEach((post) => {
+            result += post.author +': ' + post.content + calcTimeDifference(post.time) + '\n'
+        });
+        return result
+    } else {
+        return errormessages.userNotFound(name)
+    }
 }
 
 
 /**
- * Adds the user (nameToFollow) to the list following in user (name)
+ * Adds the user (nameSubscribeTo) to the list following in user (name)
  * @param {string} name 
- * @param {string} nameToFollow 
+ * @param {string} nameSubscribeTo 
  */
-const follow = (name, nameToFollow) => {
+const follow = (name, nameSubscribeTo) => {
     const allUsers = users.loadUsers()
 
     const user = allUsers.find((user) => user.name === name)
-    const userToFollow = allUsers.find((user) => user.name === nameToFollow)
+    const userToFollow = allUsers.find((user) => user.name === nameSubscribeTo)
+    const userAlreadyFollowing = user.following.find((followingName) => followingName === nameSubscribeTo)
 
     if (!user) {
         return errormessages.userNotFound(name)
     } else if (!userToFollow) {
-        return errormessages.userNotFound(nameToFollow)
+        return errormessages.userNotFound(nameSubscribeTo)
+    } else if (userAlreadyFollowing) {
+        return name + ' is already subscribed to ' + nameSubscribeTo
     } else {
-        user.following.push(nameToFollow)
+        user.following.push(nameSubscribeTo)
         users.saveUsers(allUsers)
-        return name + ' in now following ' + nameToFollow + '.'
+        return name + ' in now following ' + nameSubscribeTo + '.'
     }
 }
 
@@ -72,7 +115,7 @@ const read = (name) => {
     const user = allUsers.find((user) => user.name === name)
 
     if (user) {
-        var result = ''
+        var result = 'All posts from ' + name + ':\n'
         user.posts.forEach((post) => {
             result += name +': ' + post.content + calcTimeDifference(post.time) + '\n'
         });
--- tests/actions.test.js
+++ tests/actions.test.js
@@ -1,3 +1,24 @@
+const users = require('../src/users')
+
+const userDuplicate = 'test user 1'
+const userDelete = 'test user 2'
+var saveCurrentUsers = []
+
+
+beforeAll(() => {
+    saveCurrentUsers = users.loadUsers()
+    users.removeAll()
+    users.addUser(userDuplicate)
+    users.addUser(userDelete)
+})
+
+
+afterAll(() => {
+    users.removeAll()
+    users.saveUsers(saveCurrentUsers)
+})
+
+
 test('make a post', () => {
     
 })
